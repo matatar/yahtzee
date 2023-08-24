18f6042634d2e71bcd642e1b54d12c334d5a6734
Markus Tatar <markus.tatar80@gmail.com>
2023-08-24 11:20:41 +0200

    added sheet calculation

--- components/Play.jsx
+++ components/Play.jsx
@@ -13,6 +13,30 @@ export default class Play extends React.Component {
             dieElements,
             nextTurn,
         } = this.props
+
+        //TODO: export to utils
+        const falsyToZero = (value) => {
+            let result = Number(value);
+            if(Number.isNaN(result)){
+              result = 0;
+            }
+          
+            return result;
+          };
+          
+        const currentGame = games.filter(game => game.id === currentGameId)
+        const total = falsyToZero(currentGame.ones)
+            + falsyToZero(currentGame.two)  + falsyToZero(currentGame.threes)
+            + falsyToZero(currentGame.fours) + falsyToZero(currentGame.fives)
+            + falsyToZero(currentGame.sixes)        
+        const bonus = total >= 63  ? 35 : 0
+        const totalUpper = total + bonus
+        const totalLower = falsyToZero(currentGame.threeOfAKind) + falsyToZero(currentGame.fourOfAKind)
+            + falsyToZero(currentGame.fullHouse) + falsyToZero(currentGame.smallStraight)
+            + falsyToZero(currentGame.largeStraight) + falsyToZero(currentGame.yahtzee)
+            + falsyToZero(currentGame.chance)
+        const grandTotal = totalUpper + totalLower
+
         return(
             <>
                 <div className={style.play}>
@@ -22,44 +46,37 @@ export default class Play extends React.Component {
                                 <div key={game.id} className={`${style.player} ${game.id === currentGameId && style.active}`}>{game.playerName}</div>
                         )}
                     </div>                   
-                    {
-                        this.props.games.map(game => {
-                            if (game.id === currentGameId) {
-                                return(
-                                    <div className="container">
-                                        <div className={style.sheet}>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_ones')}</div><div className={style.col2}>{t('play_onesLabel')}</div><div className={style.col1}>{game.ones}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_twos')}</div><div className={style.col2}>{t('play_twosLabel')}</div><div className={style.col1}>{game.twos}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_threes')}</div><div className={style.col2}>{t('play_threesLabel')}</div><div className={style.col1}>{game.threes}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_fours')}</div><div className={style.col2}>{t('play_foursLabel')}</div><div className={style.col1}>{game.fours}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_fives')}</div><div className={style.col2}>{t('play_fivesLabel')}</div><div className={style.col1}>{game.fives}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_sixes')}</div><div className={style.col2}>{t('play_sixesLabel')}</div><div className={style.col1}>{game.sixes}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_total')}</div><div className={style.col2}></div><div className={style.col1}>DUMMY</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_bonus')}</div><div className={style.col2}>{t('play_bonusLabel')}</div><div className={style.col1}>DUMMY</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_totalUpper')}</div><div className={style.col2}></div><div className={style.col1}>DUMMY</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_threeOfAKind')}</div><div className={style.col2}>{t('play_threeOfAKindLabel')}</div><div className={style.col1}>{game.threeOfAKind}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_fourOfAKind')}</div><div className={style.col2}>{t('play_fourOfAKindLabel')}</div><div className={style.col1}>{game.fourOfAKind}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_fullHouse')}</div><div className={style.col2}>{t('play_fullHouseLabel')}</div><div className={style.col1}>{game.fullHouse}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_smallStraight')}</div><div className={style.col2}>{t('play_smallStraightLabel')}</div><div className={style.col1}>{game.smallStraight}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_largeStraight')}</div><div className={style.col2}>{t('play_largeStraightLabel')}</div><div className={style.col1}>{game.largeStraight}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_yahtzee')}</div><div className={style.col2}>{t('play_yahtzeeLabel')}</div><div className={style.col1}>{game.yahtzee}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_chance')}</div><div className={style.col2}>{t('play_chanceLabel')}</div><div className={style.col1}>{game.chance}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_totalLower')}</div><div className={style.col2}></div><div className={style.col1}>{game.totalLower}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_totalUpper')}</div><div className={style.col2}></div><div className={style.col1}>{game.totalUpper}</div></div>
-                                            <div className={style.figure}><div className={style.col1}>{t('play_grandTotal')}</div><div className={style.col2}></div><div className={style.col1}>{game.grandTotal}</div></div>
-                                        </div>
-                                        <div className={style.dice}>
-                                            {dieElements}
-                                        </div>
-                                        <div>
-                                            {turn.rollsLeft > 0 && <button className="btn-outline" onClick={rollDice}>{t('play_rollDice')}</button>}
-                                            {turn.rollsLeft === 0 && <button className="btn-outline" onClick={nextTurn}>{t('play_nextTurn')}</button>}
-                                        </div>
-                                    </div>
-                                )
-                            }
-                        })
-                    }
+                    
+                    <div className="container">
+                        <div className={style.sheet}>
+                            <div className={style.figure}><div className={style.col1}>{t('play_ones')}</div><div className={style.col2}>{t('play_onesLabel')}</div><div className={style.col1}>{currentGame.ones}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_twos')}</div><div className={style.col2}>{t('play_twosLabel')}</div><div className={style.col1}>{currentGame.twos}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_threes')}</div><div className={style.col2}>{t('play_threesLabel')}</div><div className={style.col1}>{currentGame.threes}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_fours')}</div><div className={style.col2}>{t('play_foursLabel')}</div><div className={style.col1}>{currentGame.fours}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_fives')}</div><div className={style.col2}>{t('play_fivesLabel')}</div><div className={style.col1}>{currentGame.fives}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_sixes')}</div><div className={style.col2}>{t('play_sixesLabel')}</div><div className={style.col1}>{currentGame.sixes}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_total')}</div><div className={style.col2}></div><div className={style.col1}>{total}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_bonus')}</div><div className={style.col2}>{t('play_bonusLabel')}</div><div className={style.col1}>{bonus}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_totalUpper')}</div><div className={style.col2}></div><div className={style.col1}>{totalUpper}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_threeOfAKind')}</div><div className={style.col2}>{t('play_threeOfAKindLabel')}</div><div className={style.col1}>{currentGame.threeOfAKind}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_fourOfAKind')}</div><div className={style.col2}>{t('play_fourOfAKindLabel')}</div><div className={style.col1}>{currentGame.fourOfAKind}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_fullHouse')}</div><div className={style.col2}>{t('play_fullHouseLabel')}</div><div className={style.col1}>{currentGame.fullHouse}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_smallStraight')}</div><div className={style.col2}>{t('play_smallStraightLabel')}</div><div className={style.col1}>{currentGame.smallStraight}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_largeStraight')}</div><div className={style.col2}>{t('play_largeStraightLabel')}</div><div className={style.col1}>{currentGame.largeStraight}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_yahtzee')}</div><div className={style.col2}>{t('play_yahtzeeLabel')}</div><div className={style.col1}>{currentGame.yahtzee}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_chance')}</div><div className={style.col2}>{t('play_chanceLabel')}</div><div className={style.col1}>{currentGame.chance}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_totalLower')}</div><div className={style.col2}></div><div className={style.col1}>{totalLower}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_totalUpper')}</div><div className={style.col2}></div><div className={style.col1}>{totalUpper}</div></div>
+                            <div className={style.figure}><div className={style.col1}>{t('play_grandTotal')}</div><div className={style.col2}></div><div className={style.col1}>{grandTotal}</div></div>
+                        </div>
+                        <div className={style.dice}>
+                            {dieElements}
+                        </div>
+                        <div>
+                            {turn.rollsLeft > 0 && <button className="btn-outline" onClick={rollDice}>{t('play_rollDice')}</button>}
+                            {turn.rollsLeft === 0 && <button className="btn-outline" onClick={nextTurn}>{t('play_nextTurn')}</button>}
+                        </div>
+                    </div>
                 </div>
             </>
         )
