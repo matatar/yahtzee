779e08052919b10b1103c23fb39b517b8974233f
Markus Tatar <markus.tatar80@gmail.com>
2023-08-28 11:50:18 +0200

    adds styles for setup component

--- components/Setup.jsx
+++ components/Setup.jsx
@@ -4,12 +4,12 @@ import styles from './Setup.module.css';
 export default class Setup extends React.Component {
     render() {
         return (
-        <>
-            <div className="setup">
+        <div className='container'>
+            <div className={styles.setup}>
                 <h2 className={styles.h2}>{this.props.t('setup_player')}</h2>
                 {this.props.playerPanelElements}
             </div>
-            <div>
+            <div className={styles.setup}>
                 <form onSubmit={this.props.handlePlayerAdd}>
                     <input 
                         name="playerInput" 
@@ -18,13 +18,13 @@ export default class Setup extends React.Component {
                         value={this.props.playerName}
                         placeholder={this.props.t('setup_playerAdd')} 
                     />
-                    <button className="btn-outline" type="submit"  >
+                    <button className={`btn-outline ${styles.setupAddButton}`} type="submit"  >
                         +
                     </button>
                 </form>
                 <button className="btn-outline" onClick={this.props.handleStartButton}>{this.props.t('setup_startGame')}</button>
             </div>
-        </>
+        </div>
         )
     }
 }
